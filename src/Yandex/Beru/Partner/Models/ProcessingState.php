35ec7bb29f4c58692d50dbc69e13176b5df60fdc
Vitalia Smahlenko <vitalia.smahlenko@nixsolutions.com>
2019-11-13 16:13:41 +0200

    Update order client

--- src/Yandex/Beru/Partner/Models/ProcessingState.php
+++ src/Yandex/Beru/Partner/Models/ProcessingState.php
@@ -6,6 +6,13 @@ use Yandex\Common\ObjectModel;
 
 class ProcessingState extends ObjectModel
 {
+    const STATUS_READY = "READY";
+    const STATUS_IN_WORK = "IN_WORK";
+    const STATUS_NEED_INFO = "NEED_INFO";
+    const STATUS_REJECTED = "REJECTED";
+    const STATUS_SUSPENDED = "SUSPENDED";
+    const STATUS_OTHER = "OTHER";
+
     protected $status;
     protected $notes;
 

54ee900abbad0277dfacdbb3ad08e533c121a410
Vitalia Smahlenko <vitalia.smahlenko@nixsolutions.com>
2020-05-26 12:02:51 +0300

    Update 2.85: Added conts, deleted method for purge relation

--- src/Yandex/Beru/Partner/Clients/RelationshipClient.php
+++ src/Yandex/Beru/Partner/Clients/RelationshipClient.php
@@ -62,32 +62,6 @@ class RelationshipClient extends Client
     }
 
     /**
-     * Removes links between products on the Beru marketplace and products from catalog
-     *
-     * @see https://yandex.ru/dev/market/partner-marketplace-cd/doc/dg/reference/post-campaigns-id-offer-mapping-entries-purge-docpage/
-     *
-     * @param $campaignId
-     * @param array $params
-     * @return PostResponse
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     * @throws \Yandex\Beru\Partner\Exception\PartnerRequestException
-     * @throws \Yandex\Common\Exception\ForbiddenException
-     * @throws \Yandex\Common\Exception\UnauthorizedException
-     */
-    public function deleteRelationship($campaignId, array $params = [])
-    {
-        $resource = 'campaigns/' . $campaignId . '/offer-mapping-entries/purge.json';
-        $response = $this->sendRequest(
-            'POST',
-            $this->getServiceUrl($resource),
-            ['json' => $params]
-        );
-        $decodedResponseBody = $this->getDecodedBody($response->getBody());
-
-        return new PostResponse($decodedResponseBody);
-    }
-
-    /**
      * Returns a list of links between products on the Beru marketplace and products from catalog
      *
      * @see https://yandex.ru/dev/market/partner-marketplace/doc/dg/reference/get-campaigns-id-offer-mapping-entries-docpage/
